185ef4fbb7865d883f7b91313ee3a176c0488bf3
Nathan <casperson24315@lcsd2kids.org>
2021-05-06 08:44:14 -0600

    made a reset function, cleaned up the ui so it actually lookos good, i changed the mouse over function frome mouse over to click, so when you draw its alot easier for more cordinated drawing

--- arrays-loops/java.js
+++ arrays-loops/java.js
@@ -1,29 +1,44 @@
-const buttonChoice = document.querySelector(".button");
+ const buttonChoice = document.querySelector(".button");
 const gridContainer = document.querySelector("#gridContainer");
+const resetButton = document.querySelector(".reset");
 
- let numOfTimes = buttonChoice;
- numOfTimes.addEventListener("click", () =>{
-
-numOfTimes = prompt("grid size?");
- });
-for (let j = 0; j<numOfTimes;j++){
-    let rowContainer = document.createElement("div");
-    for(let i = 0;i<numOfTimes;i++){
-        const singleCell = document.createElement("div");
-        singleCell.textContent=".";
-        singleCell.style.color="white";
-        singleCell.classList.add("bg-red")
-        singleCell.classList.add("grid")
-        singleCell.addEventListener("mouseover", () => {
-            singleCell.style.background="   #2f525d";
-            singleCell.style.color="#2f525d"; 
-        });
-        rowContainer.appendChild(singleCell);
+ 
+buttonChoice.addEventListener("click", () => {
+    let numOfTimes = prompt("gridsize");
+    gridContainer.innerHTML="";
+    draw(numOfTimes);
+});
+function draw(numOfTimes){
+    for (let j = 0; j<numOfTimes;j++){
+        let rowContainer = document.createElement("div");
+        for(let i = 0;i<numOfTimes;i++){
+            const singleCell = document.createElement("div");
+            singleCell.textContent=".";
+            singleCell.style.color="white";
+            singleCell.classList.add("bg-red")
+            singleCell.classList.add("grid")
+            singleCell.addEventListener("click", () => {
+                singleCell.style.background="#2f525d";
+                singleCell.style.color="#2f525d"; 
+            });
+            rowContainer.appendChild(singleCell);
+        }
+        gridContainer.appendChild(rowContainer);
     }
-    gridContainer.appendChild(rowContainer);
 }
+    
 
 
+resetButton.addEventListener("click", () =>{
+    gridContainer.innerHTML="";
+    draw(16);
+    });
+
+
+//  let numOfTimes = prompt("grid size?");
+ 
+
+draw(16);
 
 
 
